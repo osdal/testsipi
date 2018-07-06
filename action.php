72eaf619987b767318a5b79af1a78df878dc2a58
Дмитрий <osdal67@gmail.com>
2018-07-06 14:11:46 +0300

    test

---
+++ action.php
@@ -0,0 +1,36 @@
+﻿<?php 
+	//$url = htmlspecialchars($_POST['name']);
+	// Remove all illegal characters from a url
+	$url = filter_var($url, FILTER_SANITIZE_URL);
+	// Validate url
+	/*if (!filter_var($url, FILTER_VALIDATE_URL) === true) {
+	    echo("$url is a valid URL");
+		} else {
+	    echo("$url is not a valid URL");
+		}*/
+	
+	//$site_url = var_dump(filter_var($url, FILTER_VALIDATE_URL));
+	//$url = filter_var($url, FILTER_VALIDATE_URL);	
+		
+ ?>
+
+ <?php
+
+$url = htmlspecialchars($_POST['name']);// Очищаем введенное значение от запрещенных символов
+	
+
+
+if (!filter_var($url, FILTER_VALIDATE_URL) === false) {//Проверка имени сайта
+    echo("Адрес сайта $url");
+} else {
+    echo("$url неверный адрес сайта");
+}
+
+
+$file_headers = get_headers($url, 0);// подготавливаем headers страницы
+echo "<pre>";
+print_r($file_headers);
+echo "</pre>";
+?>
+	
+	
