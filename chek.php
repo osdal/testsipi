2b2262c1e32366a72ec74a815534e5ad99968108
osdal <osdal67@gmail.com>
2018-07-10 11:46:01 +0300

    111

--- chek.php
+++ chek.php
@@ -1,17 +1,8 @@
 <?php
 $file_headers = get_headers($url, 0);// подготавливаем headers страницы
-/*
-if (preg_match("/200/",$file_headers[0])){
-	echo "Редиректа нет";
-} elseif (preg_match("/3??/",$file_headers[0])) {
-	echo 'Есть редирект'; 
-} else (preg_match("/5??/",$file_headers[0])) {
-	echo "Проблема на сервере";
-}endif;*/
-echo "<pre>";
-print_r($file_headers);
-echo "</pre>";
-echo "$file_headers[0]";
+
+
+//echo "$file_headers[0]";
 if (preg_match("/200/",$file_headers[0])) {
 	echo "Редиректа нет. Проверяем сайт $url";
 }elseif (preg_match("/3??/",$file_headers[0])){
@@ -40,7 +31,7 @@ if (preg_match("/200/",$file_headers[0])) {
 	    }
 }
 
-echo $redir;
+echo "Проверяем сайт: $redir";
 
 }
 ?>
